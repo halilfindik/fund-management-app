5c4e0e54fca51ce0db3d1dc84f4bfdee8b59d6d9
halilfindik <halfin@gmail.com>
2024-09-11 18:24:05 +0300

    'Textfield' deleted.

--- frontend/src/CsvImportForm.js
+++ frontend/src/CsvImportForm.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { TextField, Button, Box, Typography } from '@mui/material';
+import { Button, Box, Typography } from '@mui/material';
 import axios from 'axios';
 
 const CsvImportForm = () => {
